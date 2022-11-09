5815b00c6ab2ff334290f801fe18679a7a77d00e
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-11-10 02:46:24 +0600

    services at home added

--- index.js
+++ index.js
@@ -31,28 +31,7 @@ async function run() {
       const services = await cursor.toArray();
       res.send(services);
     });
-    // all services
-    app.get("/services", async (req, res) => {
-      const query = {};
-      const cursor = serviceCollection.find(query);
-      const services = await cursor.toArray();
-      res.send(services);
-    });
-    //single service details
-    app.get("/services/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = {_id: ObjectId(id)};
-        const service = await serviceCollection.findOne(query);
-        res.send(service);
-    });
-
-    // add review
-    app.post("/reviews", async (req, res) => {
-      const review = req.body;
-      const result = await reviewCollection.insertOne(review);
-      res.send(result);
-    });
-
+  
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
