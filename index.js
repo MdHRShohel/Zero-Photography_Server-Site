6b53ecd619019e0f78461efbb0862a35ccbefe68
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-11-10 02:45:59 +0600

    services home api added

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require("mongodb");
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jq8ed0u.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -17,16 +16,52 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run() {
+  try {
+    const serviceCollection = client
+      .db("zeroPhotography")
+      .collection("services");
+    const reviewCollection = client.db("zeroPhotography").collection("reviews");
 
+    // services at home page
+    app.get("/services-home", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query).limit(3);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    // all services
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    //single service details
+    app.get("/services/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const service = await serviceCollection.findOne(query);
+        res.send(service);
+    });
 
-app.get('/', (req, res) => {
-  res.send('Zero Photography server is running');
+    // add review
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    });
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("Zero Photography server is running");
 });
 
 app.listen(port, () => {
